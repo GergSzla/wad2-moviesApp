e8ee7f30a3c79317beb552c22eb3c24d5d2d79e7
Gergo Szilagyi <gergo.szla@gmail.com>
2020-11-23 18:10:06 +0000

    cypress fix

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -7,30 +7,30 @@ const filterByTitle = (movieList, string) =>
 const filterByGenre = (movieList, genreId) =>
   movieList.filter((m) => m.genre_ids.includes(genreId));
 
-describe("Home Page ", () => {
-  before(() => {
-    // Get movies from TMDB and store in movies variable.
-    cy.request(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-        "TMDB_KEY"
-      )}&language=en-US&include_adult=false&include_video=false&page=1`
-    )
-      .its("body")    // Take the body of HTTP response from TMDB
-      .then((response) => {
-        movies = response.results
-      })
-  })
-  beforeEach(() => {
-    cy.visit("/")
-  });
 
-  describe("Base test", () => {
-        it("displays page header", () => {
-        cy.get("h2").contains("No. Movies");
-        cy.get(".badge").contains(20);
-        });
+  describe("Home Page ", () => {
+    before(() => {
+      // Get movies from TMDB and store in movies variable.
+      cy.request(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+          "TMDB_KEY"
+        )}&language=en-US&include_adult=false&include_video=false&page=1`
+      )
+        .its("body")    // Take the body of HTTP response from TMDB
+        .then((response) => {
+          movies = response.results
+        })
+    })
+    beforeEach(() => {
+      cy.visit("/")
     });
-
+  
+    describe("Base test", () => {
+      it("displays page header", () => {
+        cy.get("h2").contains("Discover Movies");
+        cy.get(".badge").contains(20);
+      });
+    })
     describe("Filtering", () => {
         describe("By movie title" ,() => {
           it("should display movies with 'p ' in the title", () => {
@@ -56,39 +56,19 @@ describe("Home Page ", () => {
             })
           })
         })
-        it("should display nothing with 'xyz' in the title", () => {
-            const searchString = "xyz";
-            const matchingMovies = filterByTitle(movies, searchString);
-            cy.get("input").clear().type(searchString);
-            cy.get(".card").should("have.length", matchingMovies.length);
-            
-          })
-        })
-        describe("By movie genre", () => {
+        describe("By movie genre" ,() => {
             it("should display movies with the specified genre only", () => {
-              const selectedGenreId = 35;
-              const selectedGenreText = "Comedy";
-              const matchingMovies = filterByGenre(movies, selectedGenreId);
-              cy.get("select").select(selectedGenreText); 
-              cy.get(".card").should("have.length", matchingMovies.length);
-              cy.get(".card").each(($card, index) => {
-                cy.wrap($card)
-                  .find(".card-title")
-                  .should("have.text", matchingMovies[index].title);
-              });      
-            });
-        });
-        describe("By movie genre and text", () => {
-            it("should display movies with the specified genre only", () => {
-                const searchString = "Hard Kill";
                 const selectedGenreId = 35;
                 const selectedGenreText = "Comedy";
-                var matchingMovies = filterByGenre(movies, selectedGenreId);
+                const matchingMovies = filterByGenre(movies, selectedGenreId);
                 cy.get("select").select(selectedGenreText); 
-                cy.get(".card").should("have.length", matchingMovies.length); 
-                matchingMovies = filterByTitle(movies, searchString);
-                cy.get("input").clear().type(searchString);
                 cy.get(".card").should("have.length", matchingMovies.length);
-            });
-        });
+                cy.get(".card").each(($card, index) => {
+                  cy.wrap($card)
+                    .find(".card-title")
+                    .should("have.text", matchingMovies[index].title);
+        })
+      })
     })
+});
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -40,51 +40,53 @@ describe("Navigation", () => {
       cy.get("h2").contains("Favorite Movies");
       cy.get("nav").find("li").eq(1).find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Upcoming Movies");
       cy.get("nav").find("li").eq(2).find("a").click();
       cy.get("nav.navbar-brand").find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Discover Movies");
     });
   });
-  // describe("From the Movie Details page ", () => {
-  //   beforeEach(() => {
-  //     cy.visit(`/movies/${movieId}`);
-  //   });
-  //   it("should change browser URL when show/hide reviews is clicked", () => {
-  //     cy.contains("Show Reviews").click();
-  //     cy.url().should("include", `/movies/${movieId}/reviews`);
-  //     cy.contains("Hide Reviews").click();
-  //     cy.url().should("not.include", `/movies/${movieId}/reviews`);
-  //   });
-  //   it("navigate to the full review page when a 'Full Review' link is clicked", () => {
-  //     // TODO
-  //   });
-  // });
-  describe("From the Favorites page", () => {
+
+ /* describe("From the Movie Details page ", () => {
     beforeEach(() => {
-      cy.visit("/");
-      cy.get(".card").eq(0).find("button").click();
-      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.visit(`/movies/${movieId}`);
     });
-    it("should navigate to the movies detail page and change the browser URL", () => {
-      cy.get(".card").eq(0).find("img").click();
-      cy.url().should("include", `/movies/${movies[0].id}`);
-      cy.get("h2").contains(movies[0].title);
-    });
-  });
-  describe("The Go Back button", () => {
-    beforeEach(() => {
-      cy.visit("/");
-    });
-    it("should navigate from home page to movie details and back", () => {
-      cy.get(".card").eq(1).find("img").click();
-      cy.get("svg[data-icon=arrow-circle-left]").click();
-      cy.url().should("not.include", `/movies`);
-      cy.get("h2").contains("No. Movies");
-    });
-    it("should navigate from favorites page to movie details and back", () => {
+    it("should change browser URL when show/hide reviews is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/movies/${movieId}/reviews`);
+      cy.contains("Hide Reviews").click();
+      cy.url().should("not.include", `/movies/${movieId}/reviews`);
+    });*/
+    it("navigate to the full review page when a 'Full Review' link is clicked", () => {
       // TODO
     });
-  });
-});
+
+    describe("From the Favorites page", () => {
+        beforeEach(() => {
+          cy.visit("/");
+          cy.get(".card").eq(0).find("button").click();
+          cy.get("nav").find("li").eq(2).find("a").click();
+        });
+        it("should navigate to the movies detail page and change the browser URL", () => {
+          cy.get(".card").eq(0).find("img").click();
+          cy.url().should("include", `/movies/${movies[0].id}`);
+          cy.get("h2").contains(movies[0].title);
+        });
+      });
+    });
+
+    describe("The Go Back button", () => {
+        beforeEach(() => {
+          cy.visit("/");
+        });
+        it("should navigate from home page to movie details and back", () => {
+          cy.get(".card").eq(1).find("img").click();
+          cy.get("svg[data-icon=arrow-circle-left]").click();
+          cy.url().should("not.include", `/movies`);
+          cy.get("h2").contains("Discover Movies");
+        });
+        it("should navigate from favorites page to movie details and back", () => {
+          // TODO
+        });
+      });
