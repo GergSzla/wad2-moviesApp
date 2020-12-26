5836c057ef4499eed4d9171c86691eac149844c5
Gergo Szilagyi <gergo.szla@gmail.com>
2020-12-26 21:24:20 +0000

    Review Form Fix

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./movieDetails.css";
 
-export default ({ movie }) => {
+export default ({ movie,action }) => {
   return (
     <>
       <h4>Overview</h4>
@@ -61,6 +61,9 @@ export default ({ movie }) => {
           </li>
         ))}
       </ul>
+
+      {action(movie)}
+
     </>
   );
 };
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -4,45 +4,48 @@ import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
 import MovieReviews from "../components/movieReviews";
 import useMovie from "../hooks/useMovie";
+import AddReviewButton from '../components/buttons/addReview'
 
 const MoviePage = props => {
   const { id } = props.match.params;
   const [movie] = useMovie(id)  // NEW
   return (
     <>
-    {movie ? (
-      <>
-        <PageTemplate movie={movie}>
-          <MovieDetails movie={movie} />
-        </PageTemplate>
-        <div className="row">
-          <div className="col-12 ">
-            {!props.history.location.pathname.endsWith("/reviews") ? (
-              <Link
-                className="btn btn-primary btn-block active"
-                to={`/movies/${id}/reviews`}
-              >
-                Show Reviews (Extracts)
-              </Link>
-            ) : (
-              <Link
-                className="btn btn-primary btn-block active"
-                to={`/movies/${id}`}
-              >
-                Hide Reviews 
-              </Link>
-            )}
+      {movie ? (
+        <>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie}
+              action={movie => <AddReviewButton movie={movie} />}
+            />
+          </PageTemplate>
+          <div className="row">
+            <div className="col-12 ">
+              {!props.history.location.pathname.endsWith("/reviews") ? (
+                <Link
+                  className="btn btn-primary btn-block active"
+                  to={`/movies/${id}/reviews`}
+                >
+                  Show Reviews (Extracts)
+                </Link>
+              ) : (
+                  <Link
+                    className="btn btn-primary btn-block active"
+                    to={`/movies/${id}`}
+                  >
+                    Hide Reviews
+                  </Link>
+                )}
+            </div>
           </div>
-        </div>
-        <Route
-          path={`/movies/:id/reviews`}
-          render={props => <MovieReviews movie={movie} {...props} />}
-        />
-      </>
-    ) : (
-      <p>Waiting for movie details</p>
-    )}
-  </>
+          <Route
+            path={`/movies/:id/reviews`}
+            render={props => <MovieReviews movie={movie} {...props} />}
+          />
+        </>
+      ) : (
+          <p>Waiting for movie details</p>
+        )}
+    </>
   );
 };
 
