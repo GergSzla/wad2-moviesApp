2de446106f572fe783229a3aa6dacfc97ff31a15
Gergo Szilagyi <gergo.szla@gmail.com>
2020-12-27 19:00:19 +0000

    hotfix -> fixed card view for followed actors

--- src/components/personCard/viewPerson.js
+++ src/components/personCard/viewPerson.js
@@ -7,23 +7,26 @@ const PersonCard = ({person, action}) => {
 
   return (
     <div className="col-sm-2">
-      <div className ="container">
+      <div className="card">
+        <div className="container">
           <Link to={`/person/trending/${person.id}`}>
-        <img id = "imagePerson"
-        className="card-img-tag center"
-        alt={person.name}
-        src={
-        person.profile_path
-        ? `https://image.tmdb.org/t/p/w500/${person.profile_path}`
-        : "./film-poster-placeholder.png"
-        }
-        />
-        </Link>
-        <div class="text-block">
-        <h4 className="card-title">{person.name}</h4>
+            <img id="imagePerson"
+              className="card-img-tag center"
+              alt={person.name}
+              src={
+                person.profile_path
+                  ? `https://image.tmdb.org/t/p/w500/${person.profile_path}`
+                  : "./film-poster-placeholder.png"
+              }
+            />
+          </Link>
+          <div className="card-body">
+          <h4 className="card-title">{person.name}</h4>            
+          </div>
+
         </div>
       </div>
-   </div>
+    </div>
   );
 };
 
